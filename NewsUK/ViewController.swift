10c769327b6169d1ad984ccbc6424e2370657880
Daniel Velikov <sampleapp05@gmail.com>
2025-11-01 19:43:18 +0200

    Initial Commit

---
+++ NewsUK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsUK
+//
+//  Created by Daniel Velikov on 11/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NewsUKTests/NewsUKTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsUKTests.swift
+//  NewsUKTests
+//
+//  Created by Daniel Velikov on 11/1/25.
+//
+
+import Testing
+@testable import NewsUK
+
+struct NewsUKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
